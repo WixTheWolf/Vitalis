86fe3c779b4464b4cd98f5cdb66d0f4eaac40b8c
Matthew S Wixted <matt@flavorfactory.net>
2026-07-21 08:53:30 -0700

    Restore transformation route with new app

--- app/transform/page.tsx
+++ app/transform/page.tsx
@@ -1,17 +1,2 @@
-"use client";
-
-import { useEffect, useMemo, useRef, useState } from "react";
-import styles from "./Transform.module.css";
-
-type Readiness = "green" | "yellow" | "red";
-type Meal = { time: string; title: string; foods: string; protein: string };
-type Template = {
-  focus: string;
-  calories: number;
-  bedtime: string;
-  workout: string[];
-  meals: Meal[];
-};
-type DayPlan = Template & {
-  date: Date;
-  day: number;
+import TransformApp from "./TransformApp";
+export default function Page(){return <TransformApp/>}
